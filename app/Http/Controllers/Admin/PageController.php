50ccbdc9aef0f45b39e76e4effcedf43cc3369b0
jawgumhkawng <nawbosco007@gmail.com>
2024-02-05 23:29:31 +0630

    admin dashboard setup

--- app/Http/Controllers/Admin/PageController.php
+++ app/Http/Controllers/Admin/PageController.php
@@ -7,8 +7,8 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    public function index()
+    public function dashboard()
     {
-        return "nice";
+        return view('admin.dashboard');
     }
 }
